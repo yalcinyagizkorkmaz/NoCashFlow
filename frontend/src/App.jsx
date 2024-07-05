a850276e4ac7d0f45ab1853e3cd19f8ac5310799
Yalçın Yağız KORKMAZ <99255561+yalcinyagizkorkmaz@users.noreply.github.com>
2024-07-05 15:42:00 +0300

    giriş-formu

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './Components/Form';
+
+
+
+function App() {
+  return (
+   <>
+   <Form></Form>
+   </>
+  );
+}
+
+export default App;
