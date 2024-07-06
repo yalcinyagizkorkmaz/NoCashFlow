bff151a381da041d57cf76f1a287faec1148202a
Yalçın Yağız KORKMAZ <99255561+yalcinyagizkorkmaz@users.noreply.github.com>
2024-07-06 12:02:53 +0300

    düzenlendi-form

--- frontend/src/Components/Form.jsx
+++ frontend/src/Components/Form.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Button, Checkbox, Form, Input } from 'antd';
+import { WechatOutlined} from '@ant-design/icons';
+
+
 
 
 const onFinish = (values) => {
@@ -11,74 +14,100 @@ const onFinishFailed = (errorInfo) => {
 };
 
 const App = () => (
-  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', height: '100vh' }}>
-    <h1>NOCASHFLOW</h1>
-    <Form
-      name="basic"
-      labelCol={{
-        span: 8,
-      }}
-      wrapperCol={{
-        span: 16,
-      }}
-      style={{
-        maxWidth: 600,
-      }}
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your username!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+  <div style={{ 
+    display: 'flex', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    flexDirection: 'column', 
+    height: '100vh', 
+    background: '#f0f2f5', 
+    padding: '20px' 
+  }}>
+   <h1 style={{ marginBottom: '25px', display: 'flex', alignItems: 'center' }}>
+      NOCASHFLOW
+      <WechatOutlined style={{ marginLeft: '10px' }} />
+    </h1>
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
 
-      <Form.Item
-        name="remember"
-        valuePropName="checked"
-        wrapperCol={{
-          offset: 8,
-          span: 16,
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      background: '#fff', 
+      padding: '40px', 
+      borderRadius: '10px', 
+      boxShadow: '0 0 15px rgba(0,0,0,0.1)',
+      maxWidth: '600px', 
+      width: '100%',
+      boxSizing: 'border-box'
+    }}>
+      <Form
+        name="basic"
+        style={{ width: '100%' }}
+        labelCol={{
+          xs: { span: 8 },
+          sm: { span: 6 },
         }}
-      >
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-
-      <Form.Item
         wrapperCol={{
-          offset: 8,
-          span: 16,
+          xs: { span: 16 },
+          sm: { span: 15 },
         }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your username!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{
+            offset: 6,
+            span: 18,
+          }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+        
+        <Form.Item
+          wrapperCol={{
+            offset: 6,
+            span: 18,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   </div>
 );
 
