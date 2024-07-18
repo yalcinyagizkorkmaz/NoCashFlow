b72057cd8e5ae3f89b3b08c1b2ff365ac802863b
burakberkaydin <102252373+burakberkaydin@users.noreply.github.com>
2024-07-18 18:00:19 +0300

    admin_panel ve  sikayet_detayı added

---
+++ frontend/src/Components/admin_panel.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import "../CSS/admin_panel.css";
+import "../CSS/header.css";
+import { Navigate, useLocation, useNavigate } from 'react-router-dom';
+const AdminPanel = () => {
+    const navigate = useNavigate();
+    return (
+        <div>
+            <div className="header">
+
+                <div className="logo">
+                    <img src="/beyaz.png" alt="logo" />
+                </div>
+                <div className="header-right">
+                    <img src="/Inset.png" alt="divider" className="divider" />
+                    <span className="username">Burak Berk Aydın</span>
+                    <button className="logout-button" >Çıkış Yap</button>
+                </div>
+            </div>
+            <br></br>
+            <div className="title">Şikayetler</div>
+            <div className="content-table">
+                <table class="table">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">TC</th>
+                            <th scope="col">İSİM</th>
+                            <th scope="col">ŞİKAYET</th>
+                            <th scope="col">TARİH</th>
+                            <th scope="col">KATEGORİ</th>
+                            <th scope="col">DURUM</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>111111111</td>
+                            <td>Döndü Dönmez</td>
+                            <td>18:13:50 tarihinde G2626 atmsinde</td>
+                            <td>15 Temmuz 2024</td>
+                            <td>ATM</td>
+                            <td>Çözüldü</td>
+                            <td>
+                                <button className="incele-button" onClick={()=>navigate("/sikayet-detay")}>İncele</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+           
+        </div>
+    );
+};
+export default AdminPanel;
