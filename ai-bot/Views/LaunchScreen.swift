5f2a30f8ca734199d00067c2e2758c9acc29c8f1
Ali Kaan <kaan.karagozgil@icloud.com>
2024-07-18 15:12:56 +0300

    ios değişkiler

---
+++ ai-bot/Views/LaunchScreen.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchScreen.swift
+//  AI BOT
+//
+//  Created by Ali Kaan Karagözgil on 18.07.2024.
+//
+
+import SwiftUI
+
+struct LaunchScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchScreen()
+}
