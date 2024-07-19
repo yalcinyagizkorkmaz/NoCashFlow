fa369c7791d7546e6bf70abf48ff327a7f31c263
Ali Kaan <kaan.karagozgil@icloud.com>
2024-07-19 14:13:58 +0300

    WelcomeScreen 0.5

---
+++ ai-bot/Views/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  AI BOT
+//
+//  Created by Ali Kaan Karagözgil on 19.07.2024.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
