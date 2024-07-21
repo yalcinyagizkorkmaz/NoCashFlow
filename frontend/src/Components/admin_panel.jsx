2a82c8eb962f74791f69286e12118baf90646ad0
Yalçın Yağız KORKMAZ <99255561+yalcinyagizkorkmaz@users.noreply.github.com>
2024-07-21 12:25:23 +0300

    admin-panel-filtreleme-barı

--- frontend/src/Components/admin_panel.jsx
+++ frontend/src/Components/admin_panel.jsx
@@ -2,6 +2,7 @@ import React, { useCallback, useState } from 'react';
 import "../CSS/admin_panel.css";
 import "../CSS/header.css";
 import filtericon from "../Png/filtericon.svg";
+import sıralaiconu from "../Png/sıralaiconu.svg";
 import { useNavigate } from 'react-router-dom';
 
 const AdminPanel = () => {
@@ -11,17 +12,30 @@ const AdminPanel = () => {
     }, [navigate]);
 
     const [category, setCategory] = useState('all');
+    const [isSortDropdownVisible, setIsSortDropdownVisible] = useState(false);
+    const [isCategoryDropdownVisible, setIsCategoryDropdownVisible] = useState(false);
+
     const handleCategoryChange = (event) => {
         setCategory(event.target.value);
+        setIsCategoryDropdownVisible(false);
     };
 
     const handleReset = () => {
         setCategory('all');
     };
 
-    const handleSort = () => {
+    const handleSort = (order) => {
         // Sıralama işlemleri burada yapılabilir
-        console.log('Sıralama işlemi gerçekleştirildi');
+        console.log(`Sıralama işlemi gerçekleştirildi: ${order}`);
+        setIsSortDropdownVisible(false);
+    };
+
+    const toggleSortDropdown = () => {
+        setIsSortDropdownVisible(!isSortDropdownVisible);
+    };
+
+    const toggleCategoryDropdown = () => {
+        setIsCategoryDropdownVisible(!isCategoryDropdownVisible);
     };
 
     return (
@@ -41,7 +55,7 @@ const AdminPanel = () => {
             <div className="filter-menu">
                 <form>
                     <div className="filter-item">
-                    <svg width="60px" height="60px" viewBox="0 0 52 61" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg width="70px" height="60px" viewBox="0 0 52 61" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <g id="Filter icon">
                                 <path id="Line" opacity="0.686151" d="M50.6119 60.092V1" stroke="white" stroke-width="0.2" stroke-linecap="square"/>
                                 <g id="filter">
@@ -50,7 +64,7 @@ const AdminPanel = () => {
                                 </g>
                             </g>
                         </svg>
-                        <select id="category-select" value={category} onChange={handleCategoryChange}>
+                        <select id="category-select" value={category} onChange={handleCategoryChange} className='select-category'>
                             <option value="all">Tüm Kategoriler</option>
                             <option value="mobilDeniz">Mobil Deniz</option>
                             <option value="bireyselKrediKartlari">Bireysel Kredi Kartları</option>
@@ -67,10 +81,21 @@ const AdminPanel = () => {
                         </select>
                     </div>
                     <div className="filter-item">
+                        <svg width="35" height="34" viewBox="0 0 21 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g id="ic-replay-24px">
+                                <path id="Path" d="M10.066 4.00981V1.47729L5.87192 4.64294L10.066 7.80858V5.27607C12.8425 5.27607 15.0989 6.97918 15.0989 9.07484C15.0989 11.1705 12.8425 12.8736 10.066 12.8736C7.28953 12.8736 5.03311 11.1705 5.03311 9.07484H3.35547C3.35547 11.8733 6.35844 14.1399 10.066 14.1399C13.7736 14.1399 16.7766 11.8733 16.7766 9.07484C16.7766 6.27641 13.7736 4.00981 10.066 4.00981Z" fill="white"/>
+                            </g>
+                        </svg>
                         <button type="button" onClick={handleReset}>Filtreyi Sıfırla</button>
                     </div>
                     <div className="filter-item">
-                        <button type="button" onClick={handleSort}>Sırala</button>
+                        <button type="button" onClick={toggleSortDropdown}>Sırala</button>
+                        {isSortDropdownVisible && (
+                            <div className="sort-dropdown">
+                                <button type="button" onClick={() => handleSort('asc')}>Artan</button>
+                                <button type="button" onClick={() => handleSort('desc')}>Azalan</button>
+                            </div>
+                        )}
                     </div>
                 </form>
             </div>
