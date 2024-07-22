a4b12055d7c79ae3fba17aab51b3a42e9ca23485
Ali Kaan <kaan.karagozgil@icloud.com>
2024-07-22 09:24:46 +0300

    userlogin

---
+++ ai-bot/Views/UserLogin.swift
@@ -0,0 +1,18 @@
+//
+//  UserLogin.swift
+//  AI BOT
+//
+//  Created by Ali Kaan Karagözgil on 22.07.2024.
+//
+
+import SwiftUI
+
+struct UserLogin: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserLogin()
+}
